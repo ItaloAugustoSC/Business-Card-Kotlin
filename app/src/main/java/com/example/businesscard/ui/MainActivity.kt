89ef72103937508a1c34988212b8b359670d715f
Italo <italo.augusto.ct@gmail.com>
2021-10-19 23:31:19 -0300

    Finished AddBusinessCard view and set listeners.

--- app/src/main/java/com/example/businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/ui/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.businesscard.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.businesscard.R
 import com.example.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +12,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        insertListener()
+    }
+
+    private fun insertListener(){
+        binding.addButton.setOnClickListener(){
+            val intent = Intent(this@MainActivity, AddBusinessCard::class.java)
+            startActivity(intent)
+        }
     }
 }
