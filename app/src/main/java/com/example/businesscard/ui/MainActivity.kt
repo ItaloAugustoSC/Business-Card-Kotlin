67e37e7bd134c861eae089d627abfe69c3e91726
Italo <italo.augusto.ct@gmail.com>
2021-10-20 00:33:26 -0300

    Finished the implementation of BusinessCard's database.

--- app/src/main/java/com/example/businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/ui/MainActivity.kt
@@ -3,15 +3,24 @@ package com.example.businesscard.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.businesscard.App
 import com.example.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val mainViewModel : MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+
+    private val adapter by lazy { BusinessCardAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.rvCards.adapter = adapter
+        getAllBusinessCard()
         insertListener()
     }
 
@@ -20,5 +29,11 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, AddBusinessCard::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun getAllBusinessCard() {
+        mainViewModel.getAll().observe(this, {businessCards ->
+            adapter.submitList(businessCards)
+        })
     }
 }
